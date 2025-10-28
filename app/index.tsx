fe5895b6d757245353d3bb30278590bf73093876
Douglas Lowder <douglowder@mac.com>
2025-10-27 19:01:29 -0700

    Fix up home screen markup to show the bug

--- app/index.tsx
+++ app/index.tsx
@@ -1,18 +1,51 @@
 import { useVideoPlayer, VideoView } from 'expo-video';
-import { Pressable, Text, View } from 'react-native';
-
+import { Pressable, Text, TVFocusGuideView, View } from 'react-native';
 
 export default function HomeScreen() {
-  const player = useVideoPlayer({})
-  return (<>
-    <VideoView style={{flex: 1, height: 200, width: 200}} player={player} focusable={false} nativeControls={false} isTVSelectable={false} hasTVPreferredFocus={false}/>
-    <Pressable>
-      {({focused}) => (
-        <View style={{backgroundColor: focused ? 'blue' : 'gray', padding: 10, marginTop: 20}}>
-          <Text style={{color: focused ? 'white' : 'black'}}>{focused ? 'Focused' : 'Not Focused'}</Text>
-        </View>
-      )}
-    </Pressable>
-  </>);
+  const player = useVideoPlayer({});
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Pressable>
+        {({ focused }) => (
+          <View
+            style={{
+              backgroundColor: focused ? 'blue' : 'gray',
+              padding: 10,
+              margin: 20,
+            }}
+          >
+            <Text style={{ color: focused ? 'white' : 'black' }}>
+              {focused ? 'Focused' : 'Not Focused'}
+            </Text>
+          </View>
+        )}
+      </Pressable>
+      <TVFocusGuideView focusable={false}>
+        <VideoView
+          style={{
+            height: 200,
+            width: 200,
+            borderColor: 'red',
+            borderWidth: 1,
+          }}
+          player={player}
+        />
+      </TVFocusGuideView>
+      <Pressable>
+        {({ focused }) => (
+          <View
+            style={{
+              backgroundColor: focused ? 'blue' : 'gray',
+              padding: 10,
+              marginTop: 20,
+            }}
+          >
+            <Text style={{ color: focused ? 'white' : 'black' }}>
+              {focused ? 'Focused' : 'Not Focused'}
+            </Text>
+          </View>
+        )}
+      </Pressable>
+    </View>
+  );
 }
-
